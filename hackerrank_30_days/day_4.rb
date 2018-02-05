56a5f467069957155e20716f3df546f8096f0423
Matthew Buckner <bucknermr@gmail.com>
2018-02-04 21:38:27 -0800

    Solve day 4

--- hackerrank_30_days/day_4.rb
+++ hackerrank_30_days/day_4.rb
@@ -1,13 +1,27 @@
 # https://www.hackerrank.com/challenges/30-class-vs-instance/problem
 
-T=gets.to_i
-for i in (1..T)do
-    age=gets.to_i
-    p=Person.new(age)
-    p.amIOld()
-    for j in (1..3)do
-        p.yearPasses()
+class Person
+    attr_accessor :age
+    def initialize(initialAge)
+        # Add some more code to run some checks on initialAge
+        if initialAge < 0
+            puts "Age is not valid, setting age to 0."
+            initialAge = 0
+        end
+        @age = initialAge
     end
-    p.amIOld
-  	puts ""
-end      
+    def amIOld()
+      # Do some computations in here and print out the correct statement to the console
+        if age < 13
+            puts 'You are young.'
+        elsif age < 18
+            puts 'You are a teenager.'
+        else
+            puts 'You are old.'
+        end
+    end
+    def yearPasses()
+      # Increment the age of the person in here
+        @age += 1
+    end
+end
