d18506c1e08f78c5c059cc53a773794fcd8dbf74
Matthew Buckner <bucknermr@gmail.com>
2018-02-10 16:37:36 -0800

    Solve day 6

--- hackerrank_30_days/day_6.rb
+++ hackerrank_30_days/day_6.rb
@@ -1,3 +1,21 @@
 # https://www.hackerrank.com/challenges/30-review-loop/problem
 
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+n = gets.to_i
+strings = []
+n.times do
+  strings << gets.chomp
+end
+
+strings.each do |str|
+  even = ''
+  odd = ''
+  str.each_char.with_index do |ch, i|
+    if i.even?
+      even += ch
+    else
+      odd += ch
+    end
+  end
+  puts "#{even} #{odd}"
+end
