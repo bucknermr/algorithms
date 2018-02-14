0554ec7a4bcefcb7fd13b41f8ede0e6c9a1b773e
Matthew Buckner <bucknermr@gmail.com>
2018-02-13 20:19:38 -0800

    Add day 9

---
+++ hackerrank_30_days/day_9.rb
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/30-recursion/problem
+
+#!/bin/ruby
+
+def factorial(n)
+    # Complete this function
+end
+
+n = gets.strip.to_i
+result = factorial(n)
+puts result
