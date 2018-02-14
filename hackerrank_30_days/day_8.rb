e40ff0dee04d358b89c282bf8de076c10b06d44d
Matthew Buckner <bucknermr@gmail.com>
2018-02-13 20:09:09 -0800

    Solve day 8

--- hackerrank_30_days/day_8.rb
+++ hackerrank_30_days/day_8.rb
@@ -1,3 +1,17 @@
 # https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem
 
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = gets.to_i
+phone_book = {}
+n.times do
+    line = gets.chomp.split(' ')
+    phone_book[line[0]] = line[1]
+end
+
+name = gets
+while name
+    name = name.chomp
+    puts phone_book.key?(name) ? "#{name}=#{phone_book[name]}" : 'Not found'
+    name = gets
+end
