dce9114107d6312bf960d167d1cedf76eab83347
Matthew Buckner <bucknermr@gmail.com>
2018-02-16 22:33:22 -0800

    Solve day 10

--- hackerrank_30_days/day_10.rb
+++ hackerrank_30_days/day_10.rb
@@ -3,3 +3,21 @@
 #!/bin/ruby
 
 n = gets.strip.to_i
+
+result = ""
+counter = 0
+max = 0
+
+while n > 0
+    result += (n % 2).to_s
+    if result[-1] == '1'
+        counter += 1
+        max = counter > max ? counter : max
+    else
+        counter = 0
+    end
+
+    n = n / 2
+end
+
+puts max
