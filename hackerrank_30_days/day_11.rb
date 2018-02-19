2d45de398fa452b19b79f9250f7fdc4499fe1495
Matthew Buckner <bucknermr@gmail.com>
2018-02-18 20:00:16 -0800

    Add day 11

---
+++ hackerrank_30_days/day_11.rb
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/30-2d-arrays/problem
+
+#!/bin/ruby
+
+arr = Array.new(6)
+for arr_i in (0..6-1)
+    arr_t = gets.strip
+    arr[arr_i] = arr_t.split(' ').map(&:to_i)
+end
