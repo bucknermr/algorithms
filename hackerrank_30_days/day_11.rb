d40e7d294ba43812f894f3e63228c345e9c29fdd
Matthew Buckner <bucknermr@gmail.com>
2018-02-18 20:02:24 -0800

    Begin solution for day 11

--- hackerrank_30_days/day_11.rb
+++ hackerrank_30_days/day_11.rb
@@ -7,3 +7,17 @@ for arr_i in (0..6-1)
     arr_t = gets.strip
     arr[arr_i] = arr_t.split(' ').map(&:to_i)
 end
+
+max = 0
+
+(arr.length - 2).times do |i|
+    (arr[i].length - 2).times do |j|
+        sum = 0
+        sum += arr[i][j..j + 2].inject(0, :+)
+        sum += arr[i + 1][j + 1]
+        sum += arr[i + 2][j..j + 2].inject(0, :+)
+        max = sum > max ? sum : max
+    end
+end
+
+puts max
