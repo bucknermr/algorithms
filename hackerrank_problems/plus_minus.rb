4555f3d99cfc4c36f35996270fe83364d1b4ab39
Matthew Buckner <bucknermr@gmail.com>
2018-02-20 18:51:53 -0800

    Add plus minus problem

---
+++ hackerrank_problems/plus_minus.rb
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/plus-minus/problem
+
+#!/bin/ruby
+
+def plusMinus(arr)
+    # Complete this function
+end
+
+n = gets.strip.to_i
+arr = gets.strip
+arr = arr.split(' ').map(&:to_i)
+plusMinus(arr)
