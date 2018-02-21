c94361f6901dd22905eae7265e2abd33b0ca1c99
Matthew Buckner <bucknermr@gmail.com>
2018-02-20 18:57:10 -0800

    Solve plus minus problem

--- hackerrank_problems/plus_minus.rb
+++ hackerrank_problems/plus_minus.rb
@@ -4,6 +4,23 @@
 
 def plusMinus(arr)
     # Complete this function
+    pos_count = 0
+    neg_count = 0
+    zero_count = 0
+
+    arr.each do |num|
+        if num < 0
+            neg_count += 1
+        elsif num > 0
+            pos_count += 1
+        else
+            zero_count += 1
+        end
+    end
+
+    puts (pos_count.fdiv arr.length).round(6)
+    puts (neg_count.fdiv arr.length).round(6)
+    puts (zero_count.fdiv arr.length).round(6)
 end
 
 n = gets.strip.to_i
