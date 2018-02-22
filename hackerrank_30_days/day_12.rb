cfc98138c3c2d8987342f238abf56d39ab15d80c
Matthew Buckner <bucknermr@gmail.com>
2018-02-21 22:45:32 -0800

    Solve day 12 ruby

--- hackerrank_30_days/day_12.rb
+++ hackerrank_30_days/day_12.rb
@@ -11,10 +11,33 @@ class Student <Person
     #
     # Write your constructor here
 
+    def initialize(firstName, lastName, id, scores)
+        @firstName = firstName
+        @lastName = lastName
+        @id = id
+        @scores = scores
+    end
 
     #   Function Name: calculate
     #   Return: A character denoting the grade.
     #
     # Write your function here
 
+    def calculate
+        average = @scores.inject(0, :+).fdiv(@scores.length)
+        case average
+        when (90..100)
+            'O'
+        when (80...90)
+            'E'
+        when (70...80)
+            'A'
+        when (55...70)
+            'P'
+        when (40...55)
+            'D'
+        else
+            'T'
+        end
+    end
 end
