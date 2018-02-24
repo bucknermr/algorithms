bdcbb2b5bf2759c749e9cfe618511a256ef14dcc
Matthew Buckner <bucknermr@gmail.com>
2018-02-23 23:59:28 -0800

    Solve day 13

--- hackerrank_30_days/day_13.js
+++ hackerrank_30_days/day_13.js
@@ -1,30 +1,30 @@
-# https://www.hackerrank.com/challenges/30-abstract-classes/problem
-
-'use strict';
-
-var _input = '';
-var _index = 0;
-process.stdin.on('data', (data) => { _input += data; });
-process.stdin.on('end', () => {
-    _input = _input.split(new RegExp('\n'));
-    main();
-});
-function readLine() { return _input[_index++]; }
-
-/**** Ignore above this line. ****/
-
-class Book {
-    constructor(title, author) {
-        if (this.constructor === Book) {
-            throw new TypeError('Do not attempt to directly instantiate an abstract class.');
-        }
-        else {
-            this.title = title;
-            this.author = author;
-        }
+// Declare your class here.
+class MyBook extends Book {
+    constructor(title, author, price) {
+        super(title, author);
+        this.price = price;
     }
 
     display() {
-        console.log('Implement the \'display\' method!')
+        console.log(`Title: ${this.title}`);
+        console.log(`Author: ${this.author}`);
+        console.log(`Price: ${this.price}`);
     }
 }
+    /**
+    *   Class Constructor
+    *
+    *   @param title The book's title.
+    *   @param author The book's author.
+    *   @param price The book's price.
+    **/
+    // Write your constructor here
+
+    /**
+    *   Method Name: display
+    *
+    *   Print the title, author, and price in the specified format.
+    **/
+    // Write your method here
+
+// End class
