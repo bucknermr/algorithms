80d754943b12b555883682348482245b5e034d96
Matthew Buckner <bucknermr@gmail.com>
2018-02-26 19:14:34 -0800

    Solve grading students problem

--- hackerrank_problems/grading_students.rb
+++ hackerrank_problems/grading_students.rb
@@ -4,6 +4,14 @@
 
 def solve(grades)
     # Complete this function
+  grades.map do |grade|
+    if grade < 38
+      grade
+    else
+      next_highest_5 = grade + (5 - (grade % 5))
+      (next_highest_5 - grade < 3) ? next_highest_5 : grade
+    end
+  end
 end
 
 n = gets.strip.to_i
