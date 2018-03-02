4da3221bfb79fe4658cc5a836adb17d0ed5ead21
Matthew Buckner <bucknermr@gmail.com>
2018-03-01 16:00:09 -0800

    Add optimized solution to longest substring without repeats

--- leetcode/longest_substring_without_repeats.js
+++ leetcode/longest_substring_without_repeats.js
@@ -4,31 +4,52 @@
 * @param {string} s
 * @return {number}
 */
+
+
+// Optimized solution:
+
 var lengthOfLongestSubstring = function(s) {
-  if (s.length <= 1) { return s.length; }
+    const chars = {};
+    let max = 0;
 
-  const chars = { [s[0]]: 1};
-
-  let max = 1;
-
-  let i = 0;
-  let j = 1;
-
-  while(j < s.length) {
-    let letter = s[j];
-    chars[letter] = ++chars[letter] || 1;
-    if (chars[letter] > 1) {
-      while(s[i] !== letter) {
-        chars[s[i]]--;
-        i++;
-      }
-      chars[s[i]]--;
-      i++;
+    for (let i = 0, j = 0; j < s.length; j++) {
+        const letter = s[j];
+        if (chars.hasOwnProperty(letter)) {
+            i = Math.max(i, chars[letter] + 1);
+        }
+        chars[letter] = j;
+        max = Math.max(max, j - i + 1);
     }
-    const len = (j - i + 1);
-    max = len > max ? len : max;
-    j++;
-  }
 
-  return max;
+    return max;
 };
+
+
+// var lengthOfLongestSubstring = function(s) {
+//   if (s.length <= 1) { return s.length; }
+//
+//   const chars = { [s[0]]: 1};
+//
+//   let max = 1;
+//
+//   let i = 0;
+//   let j = 1;
+//
+//   while(j < s.length) {
+//     let letter = s[j];
+//     chars[letter] = ++chars[letter] || 1;
+//     if (chars[letter] > 1) {
+//       while(s[i] !== letter) {
+//         chars[s[i]]--;
+//         i++;
+//       }
+//       chars[s[i]]--;
+//       i++;
+//     }
+//     const len = (j - i + 1);
+//     max = len > max ? len : max;
+//     j++;
+//   }
+//
+//   return max;
+// };
