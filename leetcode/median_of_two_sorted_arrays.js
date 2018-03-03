7a3bd8db0726e9b6c5c72dd4934a6cfd483c0f0a
Matthew Buckner <bucknermr@gmail.com>
2018-03-02 22:41:13 -0800

    Remove extraneous count variable

--- leetcode/median_of_two_sorted_arrays.js
+++ leetcode/median_of_two_sorted_arrays.js
@@ -10,7 +10,6 @@ var findMedianSortedArrays = function(nums1, nums2) {
 
   let i = 0;
   let j = 0;
-  let count = 0;
   let prev;
 
   while (true) {
@@ -29,3 +28,29 @@ var findMedianSortedArrays = function(nums1, nums2) {
     }
   }
 };
+// var findMedianSortedArrays = function(nums1, nums2) {
+//   let length = nums1.length + nums2.length;
+//   let isEven = length % 2 === 0;
+//   let target = Math.floor(length / 2);
+//
+//   let i = 0;
+//   let j = 0;
+//   let count = 0;
+//   let prev;
+//
+//   while (true) {
+//     if (j >= nums2.length || nums1[i] < nums2[j]) {
+//       if (i + j === target) {
+//         return isEven ? (prev + nums1[i]) / 2 : nums1[i];
+//       }
+//       prev = nums1[i];
+//       i++;
+//     } else {
+//       if (i + j === target) {
+//         return isEven ? (prev + nums2[j]) / 2 : nums2[j];
+//       }
+//       prev = nums2[j];
+//       j++;
+//     }
+//   }
+// };
