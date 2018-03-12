79cde921be4bbcc8457e0f67edff17607ac42d8a
Matthew Buckner <bucknermr@gmail.com>
2018-03-11 23:04:39 -0700

    Solve day 18

--- hackerrank_30_days/day_18.rb
+++ hackerrank_30_days/day_18.rb
@@ -3,4 +3,25 @@
 # create the Solution class
 class Solution
   	# complete the code
+  attr_reader :queue, :stack
+  def initialize
+    @stack = []
+    @queue = []
+  end
+
+  def push_character(ch)
+    stack << ch
+  end
+
+  def enqueue_character(ch)
+    queue << ch
+  end
+
+  def pop_character
+    stack.pop
+  end
+
+  def dequeue_character
+    stack.shift
+  end
 end
