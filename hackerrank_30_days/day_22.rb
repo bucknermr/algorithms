6edd7b04e27530ca9a5254f30e8d5df166e7a1a3
Matthew Buckner <bucknermr@gmail.com>
2018-03-14 00:43:19 -0700

    Solve day 22

--- hackerrank_30_days/day_22.rb
+++ hackerrank_30_days/day_22.rb
@@ -2,4 +2,14 @@
 
 def getHeight(root)
   #Write your code here
+  get_height_util(root, 0)
+end
+
+def get_height_util(node, current_height)
+  return current_height - 1 unless node
+
+  right_height = get_height_util(node.right, current_height + 1)
+  left_height = get_height_util(node.left, current_height + 1)
+
+  [right_height, left_height].max
 end
