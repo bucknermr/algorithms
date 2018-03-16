84583bdd78c9633daf1517e53db92f730ab1f71a
Matthew Buckner <bucknermr@gmail.com>
2018-03-15 17:34:24 -0700

    Solve day 23

--- hackerrank_30_days/day_23.rb
+++ hackerrank_30_days/day_23.rb
@@ -2,4 +2,15 @@
 
 def levelOrder(root)
   #Write your code here
+  queue = [root]
+  result = []
+  until queue.empty?
+    node = queue.shift
+    queue << node.left if node.left
+    queue << node.right if node.right
+
+    result << node.data
+  end
+
+  puts result.join(' ')
 end
