f71ac1c59f6c2d922b6d75453d2bb407823ec798
Matthew Buckner <bucknermr@gmail.com>
2018-03-16 23:22:37 -0700

    Add between two sets hackerrank problem

---
+++ hackerrank_problems/between_two_sets.rb
@@ -0,0 +1,17 @@
+# https://www.hackerrank.com/challenges/between-two-sets/problem
+
+#!/bin/ruby
+
+def getTotalX(a, b)
+    # Complete this function
+end
+
+n, m = gets.strip.split(' ')
+n = n.to_i
+m = m.to_i
+a = gets.strip
+a = a.split(' ').map(&:to_i)
+b = gets.strip
+b = b.split(' ').map(&:to_i)
+total = getTotalX(a, b)
+puts total
