fbef20f86c8f17a10952fdd376bb8f15f76ea746
Matthew Buckner <bucknermr@gmail.com>
2018-03-18 18:02:39 -0700

    Begin solution for day 24

--- hackerrank_30_days/day_24.rb
+++ hackerrank_30_days/day_24.rb
@@ -2,4 +2,19 @@
 
 def removeDuplicates(head)
   #write your code here
+  current_node = head
+  next_node = head.next
+  count = { [head.data] => true }
+
+  while next_node
+    if count[next_node.data]
+      current_node.next = next_node.next
+    else
+      count[next_node.data] = true
+    end
+    current_node = next_node
+    next_node = current_node.next
+  end
+
+  head
 end
