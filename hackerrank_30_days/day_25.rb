f8e59d62e9ee49af06dfb8f3253f85a9f7cd9b34
Matthew Buckner <bucknermr@gmail.com>
2018-03-20 14:45:37 -0700

    Solve day 25

--- hackerrank_30_days/day_25.rb
+++ hackerrank_30_days/day_25.rb
@@ -1,3 +1,23 @@
 # https://www.hackerrank.com/challenges/30-running-time-and-complexity/problem
 
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+def prime?(num)
+  return false if num < 2
+
+  i = 2
+  while i <= num / i
+    return false if num % i == 0
+    i += 1
+  end
+
+  true
+end
+
+n = gets.to_i
+
+n.times do
+  puts prime?(gets.to_i) ? "Prime" : "Not prime"
+end
