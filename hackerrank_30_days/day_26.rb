f3c80b2920d7a5f5d9f7923b9b76732ab6fe16fa
Matthew Buckner <bucknermr@gmail.com>
2018-03-22 23:40:50 -0700

    Begin solution to day 26

--- hackerrank_30_days/day_26.rb
+++ hackerrank_30_days/day_26.rb
@@ -1,3 +1,6 @@
 # https://www.hackerrank.com/challenges/30-nested-logic/problem
 
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+
+return_date = gets.chomp.split(' ')
+due_date = gets.chomp.split(' ')
