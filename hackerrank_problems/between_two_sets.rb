f1d2d28c76ac72dcfeb6c94d1c33244f84ba1bf5
Matthew Buckner <bucknermr@gmail.com>
2018-03-24 23:19:56 -0700

    Solve between two sets

--- hackerrank_problems/between_two_sets.rb
+++ hackerrank_problems/between_two_sets.rb
@@ -2,16 +2,38 @@
 
 #!/bin/ruby
 
+#
+# Complete the getTotalX function below.
+#
 def getTotalX(a, b)
-    # Complete this function
+  factors_of_b = (1..b.min).select do |num|
+    b.all? { |n| num.factor?(n) }
+  end
+
+  factors_of_b.select { |num| a.all? { |n| n.factor?(num) } }.length
 end
 
-n, m = gets.strip.split(' ')
-n = n.to_i
-m = m.to_i
-a = gets.strip
-a = a.split(' ').map(&:to_i)
-b = gets.strip
-b = b.split(' ').map(&:to_i)
-total = getTotalX(a, b)
-puts total
+class Integer
+  def factor?(other_num)
+    other_num % self == 0
+  end
+end
+
+fp = File.open(ENV['OUTPUT_PATH'], 'w')
+
+nm = gets.rstrip.split
+
+n = nm[0].to_i
+
+m = nm[1].to_i
+
+a = gets.rstrip.split(' ').map(&:to_i)
+
+b = gets.rstrip.split(' ').map(&:to_i)
+
+total = getTotalX a, b
+
+fp.write total
+fp.write "\n"
+
+fp.close()
