c438f5cdeeec9f6c34fc94f54ff2b6f7da3757ca
Matthew Buckner <bucknermr@gmail.com>
2018-03-25 23:47:09 -0700

    Add additional test cases for day 27

--- hackerrank_30_days/day_27.rb
+++ hackerrank_30_days/day_27.rb
@@ -1,9 +1,18 @@
 # https://www.hackerrank.com/challenges/30-testing/problem
 
-puts '2'
+puts '5'
 
 puts '4 3'
 puts '2 0 -2 -5'
 
 puts '9 4'
 puts '-1 1 0 0 4 5 -2 4 3'
+
+puts '3 3'
+puts '0 0 0'
+
+puts '3 3'
+puts '0 1 0'
+
+puts '5 2'
+puts '4 3 0 -1 -2'
