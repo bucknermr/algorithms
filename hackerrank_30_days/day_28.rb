ce1c07e8de64f421c8a8aa4634c28495de15bd9e
Matthew Buckner <bucknermr@gmail.com>
2018-03-31 17:05:33 -0700

    Begin solution for day 28

--- hackerrank_30_days/day_28.rb
+++ hackerrank_30_days/day_28.rb
@@ -3,6 +3,50 @@
 #!/bin/ruby
 
 N = gets.strip.to_i
+
+names = []
+
 for a0 in (0..N-1)
     firstName,emailID = gets.strip.split(' ')
+    names << firstName if gmail?(emailId)
+end
+
+def gmail?(email)
+  email.match?(/@gmail\.com$/)
+end
+
+def name_sort(names)
+  name_sort_util(names)
+end
+
+def name_sort_util(names, start_idx = 0, end_idx = names.length - 1)
+  p 'start', start_idx
+  p 'end', end_idx
+  return names if start_idx >= end_idx
+
+  pivot = names[start_idx].downcase
+  barrier = start_idx + 1
+
+  idx = start_idx + 1
+  while idx < end_idx
+    if pivot > names[idx].downcase
+      swap_elements(names, idx, barrier)
+      barrier += 1
+    end
+
+    idx += 1
+  end
+
+  swap_elements(names, start_idx, barrier)
+
+  p 'barrier', barrier
+
+  name_sort_util(names, start_idx, barrier - 1)
+  name_sort_util(names, barrier + 1, end_idx)
+
+  names
+end
+
+def swap_elements(array, i, j)
+  array[i], array[j] = array[j], array[i]
 end
