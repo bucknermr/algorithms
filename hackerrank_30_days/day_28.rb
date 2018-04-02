0da042345bdb24c1034ea9794c5053375b062710
Matthew Buckner <bucknermr@gmail.com>
2018-04-01 23:53:25 -0700

    Implement quicksort for day 28

--- hackerrank_30_days/day_28.rb
+++ hackerrank_30_days/day_28.rb
@@ -1,52 +1,46 @@
-# https://www.hackerrank.com/challenges/30-regex-patterns/problem
-
 #!/bin/ruby
+
+def gmail?(email)
+  email.match?(/@gmail\.com$/)
+end
+
+def quicksort(arr, &blk)
+  blk ||= Proc.new { |x, y| x <=> y }
+  quicksort_util(arr, &blk)
+end
+
+def quicksort_util(arr, start_idx = 0, end_idx = arr.length - 1, &blk)
+  return arr if start_idx >= end_idx
+
+  barrier = start_idx
+  pivot = arr.first
+
+  (start_idx + 1..end_idx).each do |i|
+    if blk.call(pivot, arr[i]) == 1
+      barrier += 1
+      swap_elements(arr, barrier, i)
+    end
+  end
+
+  swap_elements(arr, start_idx, barrier)
+
+  quicksort_util(arr, start_idx, barrier - 1, &blk)
+  quicksort_util(arr, barrier + 1, end_idx, &blk)
+
+  arr
+end
+
+def swap_elements(arr, i, j)
+  arr[i], arr[j] = arr[j], arr[i]
+end
 
 N = gets.strip.to_i
 
 names = []
 
 for a0 in (0..N-1)
-    firstName,emailID = gets.strip.split(' ')
-    names << firstName if gmail?(emailId)
+  firstName,emailID = gets.strip.split(' ')
+  names << firstName if gmail?(emailID)
 end
 
-def gmail?(email)
-  email.match?(/@gmail\.com$/)
-end
-
-def name_sort(names)
-  name_sort_util(names)
-end
-
-def name_sort_util(names, start_idx = 0, end_idx = names.length - 1)
-  p 'start', start_idx
-  p 'end', end_idx
-  return names if start_idx >= end_idx
-
-  pivot = names[start_idx].downcase
-  barrier = start_idx + 1
-
-  idx = start_idx + 1
-  while idx < end_idx
-    if pivot > names[idx].downcase
-      swap_elements(names, idx, barrier)
-      barrier += 1
-    end
-
-    idx += 1
-  end
-
-  swap_elements(names, start_idx, barrier)
-
-  p 'barrier', barrier
-
-  name_sort_util(names, start_idx, barrier - 1)
-  name_sort_util(names, barrier + 1, end_idx)
-
-  names
-end
-
-def swap_elements(array, i, j)
-  array[i], array[j] = array[j], array[i]
-end
+puts quicksort(names) { |x, y| x.downcase <=> y.downcase }
