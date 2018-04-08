4cd5000980abc867af1bb41a31849ec090cf5ba0
Matthew Buckner <bucknermr@gmail.com>
2018-04-07 23:42:00 -0700

    Add day 29

---
+++ hackerrank_30_days/day_29.rb
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/30-bitwise-and/problem
+
+#!/bin/ruby
+
+t = gets.strip.to_i
+for a0 in (0..t-1)
+    n,k = gets.strip.split(' ')
+    n = n.to_i
+    k = k.to_i
+end
