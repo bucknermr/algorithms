f43d8adbc2391d6828e41161117f09f7a3e40f26
Matthew Buckner <bucknermr@gmail.com>
2018-04-08 18:03:12 -0700

    Add naive solution to day 29

--- hackerrank_30_days/day_29.rb
+++ hackerrank_30_days/day_29.rb
@@ -2,9 +2,21 @@
 
 #!/bin/ruby
 
+def method(n, k)
+  max = -1.0 / 0
+  (1...n).each do |x|
+    (x + 1..n).each do |y|
+      z = x & y
+      max = z if z < k && z > max
+    end
+  end
+  max
+end
+
 t = gets.strip.to_i
 for a0 in (0..t-1)
-    n,k = gets.strip.split(' ')
-    n = n.to_i
-    k = k.to_i
+  n,k = gets.strip.split(' ')
+  n = n.to_i
+  k = k.to_i
+  puts method(n, k)
 end
